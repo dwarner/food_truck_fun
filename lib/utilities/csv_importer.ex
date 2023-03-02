cba7912229e293f4b99b7ab2849ab35091411d18
Dennis Warner <dwarner@smartpaylease.com>
2023-03-02 12:03:16 -0500

    Modifying CsvImporter to handle CsvImporter.import_locations/1

--- lib/utilities/csv_importer.ex
+++ lib/utilities/csv_importer.ex
@@ -4,6 +4,8 @@ defmodule FoodTruckFun.Utilities.CsvImporter do
 
   `csv_import` loads and sanitizes the data found by the .csv file set by `@filename`.
 
+  NOTE: It will only import records where the status is "APPROVED" and a LocationDescription is set.
+
   To import the CSV file set by @filename:
   iex> FoodTruckFun.Utilities.CsvImporter.csv_import()
   """
@@ -12,6 +14,7 @@ defmodule FoodTruckFun.Utilities.CsvImporter do
   require Logger
 
   alias FoodTruckFun.Businesses
+  alias FoodTruckFun.Locations
 
   @filename "../../data/Mobile_Food_Facility_Permit.csv"
 
@@ -21,13 +24,14 @@ defmodule FoodTruckFun.Utilities.CsvImporter do
   ## Examples
 
     iex> FoodTruckFun.Utilities.CsvImporter.csv_import()
-    [ok: %{}, ...]
+    :ok
 
   """
   def csv_import do
     csv_decoder()
     |> sanitize_records()
-    |> import_into_db()
+    |> import_businesses()
+    |> import_locations()
   end
 
   @doc """
@@ -43,13 +47,16 @@ defmodule FoodTruckFun.Utilities.CsvImporter do
     @filename
     |> Path.expand(__DIR__)
     |> File.stream!
-    |> CSV.decode(headers: true, field_transform: &String.trim/1)
+    |> CSV.decode(headers: headers(),
+                  field_transform: &String.trim/1)
     |> Enum.to_list()
+    |> List.delete_at(0)
   end
 
   @doc """
   Takes a list of records and returns a list of sanitized records:
-    * Status => "APPROVED"
+    * status => "APPROVED"
+    * location_description => !""
 
   ## Examples
 
@@ -60,40 +67,108 @@ defmodule FoodTruckFun.Utilities.CsvImporter do
   def sanitize_records(records) do
     records
     |> only_approved()
+    |> has_location_description()
   end
 
   @doc """
-  Imports a list of records into the dabase.
-  New "Applicants" are added to the `businesses` table.
+  Imports new "Applicants" into the `businesses` table.
 
   ## Examples
 
-    iex> FoodTruckFun.Utilities.CsvImporter.import_into_db(records)
-    Imported 0 businesses from csv.
-    There were 148 errors (see logs).
-    :ok
+    iex> FoodTruckFun.Utilities.CsvImporter.import_businesses(records)
+    [{:ok, 5171, %{}}, ...}]
+
   """
-  def import_into_db(records) do
-    import_results =
-      for record <- records do
-        {:ok, %{"Applicant" => name}} = record
+  def import_businesses(records) do
+    for record <- records do
+      {:ok, fields} = record
+      business_name = fields["business_name"]
 
-        case Businesses.create_business(%{name: name}) do
-          {:ok, business} ->
-            Logger.info("Business \"#{business.name}\" has been added.")
-            :ok
-          {:error, changeset} ->
-            Logger.error("Error inserting business: #{inspect(changeset)}")
-            :error
-        end
-      end |> Enum.frequencies()
+      try do
+        business = Businesses.get_business_by_name!(business_name)
+        {:ok, business.id, fields}
+      rescue
+        Ecto.NoResultsError ->
+          {:ok, business} = Businesses.create_business(%{name: fields["business_name"]})
+          {:ok, business.id, fields}
+      end
+    end
+  end
 
-    Logger.info("Imported #{import_results[:ok] || 0} businesses from csv.\nThere were #{import_results[:error] || 0} errors (see logs).")
+  @doc """
+  Imports new locations into the `locations` table.
+
+  ## Examples
+
+    iex> FoodTruckFun.Utilities.CsvImporter.import_locations(records)
+    [{:ok, 1293}, ...}]
+
+  """
+  def import_locations(records) do
+    records
+    |> Enum.filter(fn x -> :ok in Tuple.to_list(x) end)
+    |> Enum.map(fn {:ok, business_id, fields} = _x ->
+      attrs = Map.put(fields, "business_id", business_id)
+
+      date = cast_date(attrs["expiration_date"])
+      attrs = Map.put(attrs, "expiration_date", date)
+
+      try do
+        location = Locations.get_location_by_external_location_id!(attrs["external_location_id"])
+        {:ok, location.id}
+      rescue
+        Ecto.NoResultsError ->
+          {:ok, location} = Locations.create_location(attrs)
+          {:ok, location.id}
+      end
+    end)
   end
 
   defp only_approved(records) do
     Enum.filter(records, fn {:ok, record} ->
-      record["Status"] == "APPROVED"
+      record["status"] == "APPROVED"
     end)
+  end
+
+  defp has_location_description(records) do
+    Enum.filter(records, fn {:ok, record} ->
+      record["location_description"] != ""
+    end)
+  end
+
+  defp cast_date(date) do
+    [date | _tail] = String.split(date, " ")
+    [mm, dd, yyyy] = String.split(date, "/")
+    {:ok, date} = Date.from_iso8601("#{yyyy}-#{mm}-#{dd}")
+    date
+  end
+
+  defp headers() do
+    [
+      "external_location_id",
+      "business_name",
+      "facility_type",
+      "cnn",
+      "location_description",
+      "address",
+      "blocklot",
+      "block",
+      "lot",
+      "permit",
+      "status",
+      "food_items",
+      "x",
+      "y",
+      "latitude",
+      "longitude",
+      "schedule",
+      "day_hours",
+      "noi_sent",
+      "approved_on",
+      "received_on",
+      "prior_permit",
+      "expiration_date",
+      "location"
+    ]
   end
 end
--- test/utilities/csv_importer_test.exs
+++ test/utilities/csv_importer_test.exs
@@ -11,60 +11,69 @@ defmodule Utilities.CsvImporterTest do
   describe "csv_importer" do
     @records [
       ok: %{
-        "Address" => "2535 TAYLOR ST",
-        "Applicant" => "Datam SF LLC dba Anzu To You",
-        "Approved" => "11/05/2021 12:00:00 AM",
-        "ExpirationDate" => "11/15/2022 12:00:00 AM",
-        "FacilityType" => "Truck",
-        "FoodItems" => "Asian Fusion - Japanese Sandwiches/Sliders/Misubi",
-        "Latitude" => "37.805885350100986",
-        "Location" => "(37.805885350100986, -122.41594524663745)",
-        "LocationDescription" => "TAYLOR ST: BAY ST to NORTH POINT ST (2500 - 2599)",
-        "Longitude" => "-122.41594524663745",
-        "Schedule" => "http://bsm.sfdpw.org/PermitsTracker/reports/report.aspx?title=schedule&report=rptSchedule&params=permit=21MFF-00106&ExportPDF=1&Filename=21MFF-00106_schedule.pdf",
-        "Status" => "APPROVED"
+        "address" => "5 THE EMBARCADERO",
+        "business_name" => "Ziaurehman Amini",
+        "expiration_date" => "03/15/2016 12:00:00 AM",
+        "external_location_id" => "735318",
+        "facility_type" => "Push Cart",
+        "food_items" => "Pizza by the slice!",
+        "latitude" => "37.794331003246846",
+        "location_description" => "MARKET ST: DRUMM ST intersection",
+        "longitude" => "-122.39581105302317",
+        "schedule" => "http://bsm.sfdpw.org/PermitsTracker/reports/report.aspx?title=schedule&report=rptSchedule&params=permit=15MFF-0159&ExportPDF=1&Filename=15MFF-0159_schedule.pdf",
+        "status" => "EXPIRED"
       },
       ok: %{
-        "Address" => "Assessors Block 7283/Lot004",
-        "Applicant" => "Casita Vegana",
-        "Approved" => "11/05/2021 12:00:00 AM",
-        "ExpirationDate" => "11/15/2022 12:00:00 AM",
-        "FacilityType" => "Truck",
-        "FoodItems" => "Coffee: Vegan Pastries: Vegan Hot Dogs: Vegan Tamales: Te: Vegan Shakes",
-        "Latitude" => "37.72188970870838",
-        "Location" => "(37.72188970870838, -122.4925212449949)",
-        "LocationDescription" => "JOHN MUIR DR: LAKE MERCED BLVD to SKYLINE BLVD (200 - 699)",
-        "Longitude" => "-122.4925212449949",
-        "Schedule" => "http://bsm.sfdpw.org/PermitsTracker/reports/report.aspx?title=schedule&report=rptSchedule&params=permit=21MFF-00105&ExportPDF=1&Filename=21MFF-00105_schedule.pdf",
-        "Status" => "EXPIRED"
+        "address" => "2155 37TH AVE",
+        "business_name" => "Sunset Mercantile/Outer Sunset Farmers Market & Mercantile",
+        "expiration_date" => "11/15/2022 12:00:00 AM",
+        "external_location_id" => "1612654",
+        "facility_type" => "Truck",
+        "food_items" => "Multiple Food Trucks & Food Types",
+        "latitude" => "37.74732654654123",
+        "location_description" => "37TH AVE: QUINTARA ST to RIVERA ST (2100 - 2199)",
+        "longitude" => "-122.49628067270531",
+        "schedule" => "http://bsm.sfdpw.org/PermitsTracker/reports/report.aspx?title=schedule&report=rptSchedule&params=permit=22MFF-00031&ExportPDF=1&Filename=22MFF-00031_schedule.pdf",
+        "status" => "REQUESTED"
       },
       ok: %{
-        "Address" => "351 CALIFORNIA ST",
-        "Applicant" => "MOMO INNOVATION LLC",
-        "Approved" => "10/22/2021 12:00:00 AM",
-        "ExpirationDate" => "11/15/2022 12:00:00 AM",
-        "FacilityType" => "Truck",
-        "FoodItems" => "Noodles: Meat & Drinks",
-        "Latitude" => "37.792870749741496",
-        "Location" => "(37.792870749741496, -122.4007474940767)",
-        "LocationDescription" => "CALIFORNIA ST: BATTERY ST to SANSOME ST (300 - 399)",
-        "Longitude" => "-122.4007474940767",
-        "Schedule" => "http://bsm.sfdpw.org/PermitsTracker/reports/report.aspx?title=schedule&report=rptSchedule&params=permit=21MFF-00090&ExportPDF=1&Filename=21MFF-00090_schedule.pdf",
-        "Status" => "REQUESTED"
+        "address" => "185 BERRY ST",
+        "business_name" => "Off the Grid Services, LLC",
+        "expiration_date" => "11/15/2023 12:00:00 AM",
+        "external_location_id" => "1652610",
+        "facility_type" => "Truck",
+        "food_items" => "everything except for hot dogs",
+        "latitude" => "37.77632714778992",
+        "location_description" => "BERRY ST: 03RD ST to 04TH ST (100 - 199)",
+        "longitude" => "-122.39179682107691",
+        "schedule" => "http://bsm.sfdpw.org/PermitsTracker/reports/report.aspx?title=schedule&report=rptSchedule&params=permit=22MFF-00035&ExportPDF=1&Filename=22MFF-00035_schedule.pdf",
+        "status" => "SUSPEND"
       },
       ok: %{
-        "Address" => "667 MISSION ST",
-        "Applicant" => "MOMO INNOVATION LLC",
-        "Approved" => "10/22/2021 12:00:00 AM",
-        "ExpirationDate" => "11/15/2022 12:00:00 AM",
-        "FacilityType" => "Truck",
-        "FoodItems" => "Noodles: Meat & Drinks",
-        "Latitude" => "37.7865580501799",
-        "Location" => "(37.7865580501799, -122.40103337534973)",
-        "LocationDescription" => "MISSION ST: ANNIE ST to 03RD ST (663 - 699)",
-        "Longitude" => "-122.40103337534973",
-        "Schedule" => "http://bsm.sfdpw.org/PermitsTracker/reports/report.aspx?title=schedule&report=rptSchedule&params=permit=21MFF-00090&ExportPDF=1&Filename=21MFF-00090_schedule.pdf",
-        "Status" => "SUSPEND"
+        "address" => "430 CALIFORNIA ST",
+        "business_name" => "Bonito Poke",
+        "expiration_date" => "11/15/2023 12:00:00 AM",
+        "external_location_id" => "1658690",
+        "facility_type" => "Truck",
+        "food_items" => "Bonito Poke Bowls & Various Drinks",
+        "latitude" => "37.793262206923096",
+        "location_description" => "CALIFORNIA ST: SANSOME ST to LEIDESDORFF ST (400 - 448)",
+        "longitude" => "-122.4017890913628",
+        "schedule" => "http://bsm.sfdpw.org/PermitsTracker/reports/report.aspx?title=schedule&report=rptSchedule&params=permit=22MFF-00070&ExportPDF=1&Filename=22MFF-00070_schedule.pdf",
+        "status" => "APPROVED"
+      },
+      ok: %{
+        "address" => "185 BERRY ST",
+        "business_name" => "Off the Grid Services, LLC",
+        "expiration_date" => "11/15/2023 12:00:00 AM",
+        "external_location_id" => "1652620",
+        "facility_type" => "Truck",
+        "food_items" => "everything except for hot dogs",
+        "latitude" => "37.77632714778992",
+        "location_description" => "",
+        "longitude" => "-122.39179682107691",
+        "schedule" => "http://bsm.sfdpw.org/PermitsTracker/reports/report.aspx?title=schedule&report=rptSchedule&params=permit=22MFF-00036&ExportPDF=1&Filename=22MFF-00036_schedule.pdf",
+        "status" => "APPROVED"
       }
     ]
 
@@ -72,8 +81,18 @@ defmodule Utilities.CsvImporterTest do
       assert CsvImporter.sanitize_records(@records) |> length == 1
     end
 
-    test "import_into_db/1 only imports businsesses not already in the db" do
-     assert :ok == CsvImporter.import_into_db(@records)
+    test "import_businesses/1 imports businsesses into the db" do
+      assert CsvImporter.import_businesses(@records) |> length == 5
+    end
+
+    test "import_locations/1 imports locations into the db" do
+      locations = @records
+                  |> CsvImporter.sanitize_records()
+                  |> CsvImporter.import_businesses()
+                  |> CsvImporter.import_locations()
+                  |> length
+
+      assert locations == 1
     end
   end
 end
