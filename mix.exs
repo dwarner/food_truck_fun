016cb7b22b570b75b6f2cf918f51624c63f87619
Dennis Warner <dwarner@smartpaylease.com>
2023-03-03 09:00:45 -0500

    Adding EctoDbg
    https://github.com/akoutmos/ecto_dbg

--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule FoodTruckFun.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:csv, "~> 3.0"},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:ecto_dbg, "~> 0.1.0", only: [:dev, :test]}
     ]
   end
 
